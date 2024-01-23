5a174457851583067d81b2c808f73cc516155697
Mohammed Aloshaibi <mohammed.aloshaiby@gmail.com>
2024-01-23 17:23:41 +0300

    first commit

---
+++ overriding VS hiding/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class BaseClass
+{
+    public virtual void Print()
+    {
+        Console.WriteLine("Base class to prit");
+    }
+}
+
+public class DerivedClass : BaseClass
+{
+    public override void Print()
+    {
+        Console.WriteLine("Derived class to Print");
+    }
+}
+
+
+
+
+public class Program
+{
+    public static void  Main(string[] args)
+    {
+        BaseClass B = new DerivedClass();
+        B.Print();
+
+        //BaseClass D = new BaseClass();
+        //((BaseClass)D).Print();
+
+
+    }
+}
